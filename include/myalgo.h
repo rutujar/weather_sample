e082d37c587bd9e06cb1f82b53a6e66dfd8c223a
rutujar <rutujarajesh23@gmail.com>
2018-08-12 15:22:10 +0530

    Create myalgo.h

---
+++ include/myalgo.h
@@ -0,0 +1,16 @@
+#ifndef __MYALGO_H
+#define __MYALGO_H
+
+template <typename T>
+void gswap(T&, T&);
+
+template <typename T>
+bool gcompare(T&, T&);
+
+template <typename T>
+T gmin(T&, T&);
+
+template <typename T>
+T gmax(T&, T&);
+
+#endif
